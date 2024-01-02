2abf94dd0e032e4ec93596d3836712d6e9108770
Ram Yadav <ram902215@gmail.com>
2024-01-01 21:10:40 -0800

    day1

---
+++ day1.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // variable rule:-
+    // 1.not start with number
+    // 2.only start with letter,_,$
+    // 3.variable name is not be keyword
+    // 4.variable name can't be take space
+    // Syantax:- data_type variable name;
+    // ex.int num;
+
+    // data_type:-
+    // 1.primary data_type:-[int,char,float,double,void,short,boolean,long]
+    // 2.derived data_type:-[array,pointer,funtion,refernce]
+    // 3.user defined data_type:-[struct,enum,uninon,class]
+
+
+
+    // Arithematic Operator
+    // 1.Addition(+)
+    // 2.Subtract(-)
+    // 3.Multiply(*)
+    // 4.Division(/)
+    // 5.Modulus(%)
+
+
+    // int num1=20,add,sub,mul,div,mod;
+    // int num2=10;
+    // add=num1+num2;
+    // sub=num1-num2;
+    // mul=num1*num2;
+    // div=num1/num2;
+    // mod=num1%num2;
+    // cout<<"Addition of two numbers:"<<add<<endl;
+    // cout<<"Subtraction of two numbers:"<<sub<<endl;
+    // cout<<"Multiplication of two numbers:"<<mul<<endl;
+    // cout<<"Devision of two numbers:"<<div<<endl;
+    // cout<<"Modulus of two numbers:"<<mod<<endl;
+
+
+    //Assignment Operator:[=,+=,-=,*=,/=,%=]
+
+    //+=
+    // int num=10;
+    // num+=10;
+    // cout<<num;
+
+    // -=
+    // int num=10;
+    // num-=10;
+    // cout<<num;
+
+
+
+    //ternary operator
+    // ?:   Syntax:   (codition)?true:false
+    int num1=10;
+    int num2=20;
+    int num3=30;
+    int max=(num1>num2)?((num1>num3)?num1:num3):((num2>num3)?num2:num3);
+    cout<<"Maximum number:"<<max;
+
+}
