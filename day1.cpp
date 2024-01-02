7db6241a97eb600f8f0f8e673d981b7d96380c1a
Ram Yadav <ram902215@gmail.com>
2024-01-01 21:58:27 -0800

    day1

--- day1.cpp
+++ day1.cpp
@@ -54,10 +54,38 @@ int main(){
 
     //ternary operator
     // ?:   Syntax:   (codition)?true:false
-    int num1=10;
-    int num2=20;
-    int num3=30;
-    int max=(num1>num2)?((num1>num3)?num1:num3):((num2>num3)?num2:num3);
-    cout<<"Maximum number:"<<max;
+    // int num1=10;
+    // int num2=20;
+    // int num3=30;
+    // int max=(num1>num2)?((num1>num3)?num1:num3):((num2>num3)?num2:num3);
+    // cout<<"Maximum number:"<<max;
+
+
+
+    // unary operator [++(pre increment or post increment),--(pre decrement or post decrement)]
+    // int num=10;
+    // cout<<num--<<endl;
+    // cout<<num;
+
+
+
+
+    // relational operator[<,>,<=,>=,!=]
+    // int num1=10,num2=10;
+    // cout<<(num1!=num2);
+
+
+
+    // logical operator[&&,||,!]
+    // bool num1=true;
+    // bool num2=false;
+    // cout<<!(num1);
+
+
+
+    // bitwise operator [&,|,~,<<,>>,^]
+    // int num1=14;   // 0011
+    // int num2=6;    // 0110
+    // cout<<(num1>>2);
 
 }
